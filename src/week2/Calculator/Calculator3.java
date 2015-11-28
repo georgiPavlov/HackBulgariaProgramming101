de9d65261e464c217c350f2607789091d857d42c
georgiPavlov <georgi95.bg@gmail.com>
2015-11-29 00:49:44 +0200

    week2

--- src/week2/Calculator/Calculator3.java
+++ src/week2/Calculator/Calculator3.java
@@ -6,11 +6,13 @@ public class Calculator3 {
         Calculator3 calculator3 = new Calculator3();
         System.out.println("Enter expression:> ");
         Scanner scanner = new Scanner(System.in);
-        calculator3.operation1=scanner.nextLine();
+        //calculator3.operation1=scanner.nextLine();
+        calculator3.operation1="6*2";
         System.out.println("Output:> ");
         System.out.print(calculator3.firstStage());
     }
-    String operation1,operation;
+    String operation1;
+    String operation;
     public String firstStage(){
         StringBuilder tempBuld = new StringBuilder();
         pa:for (int i = 0; i < operation1.length(); i++) {
@@ -67,10 +69,11 @@ public class Calculator3 {
 
     public String calculate(String calc){
         double sum=0;
-        int i=1,j=1;
+        int i=1;
+        int j=1;
         boolean b1=false;
         String[] parts = calc.split("[+-]+");
-        String[] operators = calc.split("[0-9.]+");
+        String[] operators = calc.split("[\\*//0-9.]+");
         if(calc.charAt(0) == '-' || calc.charAt(0) == '+') {
             b1 = true;
             i = 2;
@@ -85,15 +88,18 @@ public class Calculator3 {
             }
         }
         if(b1){
+            parts[1] = MultiDiv(parts[1]);
             sum -= Double.parseDouble(parts[1]);
         }else {
+            parts[0] = MultiDiv(parts[0]);
             sum += Double.parseDouble(parts[0]);
         }
         return Double.toString(sum);
     }
 
     private String MultiDiv(String calc){
-        int i=1,j=1;
+        int i=1;
+        int j=1;
         String[] parts = calc.split("[\\*//]+");
         String[] operators = calc.split("[0-9.]+");
         double sum= Double.parseDouble(parts[0]);
