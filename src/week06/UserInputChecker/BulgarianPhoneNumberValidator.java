0e24dff173b0e8284bebebb3a66f347f45d8903a
georgiPavlov <georgi95.bg@gmail.com>
2016-01-07 23:38:27 +0200

    update

--- src/week06/UserInputChecker/BulgarianPhoneNumberValidator.java
+++ src/week06/UserInputChecker/BulgarianPhoneNumberValidator.java
@@ -1,5 +1,8 @@
 package week06.UserInputChecker;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by georgipavlov on 06.01.16.
  */
@@ -7,15 +10,18 @@ public class BulgarianPhoneNumberValidator {
     private static final String NAME = "Jonny";
     boolean result;
     UserInputChecker.Validator t;
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String USERNAME_PATTERN = " ^(\\+359)+([0-9]{7,15})$";
 
     public boolean PersonNameValidator1(String i){
         t = new UserInputChecker.Validator() {
             @Override
             public boolean validate(String input) {
-                if(input.equals(NAME)){
-                    return true;
-                }
-                return false;
+                pattern = Pattern.compile(USERNAME_PATTERN);
+                final String inP = input;
+                matcher = pattern.matcher(inP);
+                return matcher.matches();
             }
         };
         return t.validate(i);
