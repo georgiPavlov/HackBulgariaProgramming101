cec708473a304b523f166b77aca545b320f28d89
georgiPavlov <georgi95.bg@gmail.com>
2016-01-08 14:51:29 +0200

    update to bonus :) :*

--- src/week06/BinaryTree/BinaryTree.java
+++ src/week06/BinaryTree/BinaryTree.java
@@ -6,14 +6,14 @@ package week06.BinaryTree;
 public class BinaryTree<T extends Comparable<T>> {
 
     private static class BinaryTreeNode<T extends Comparable<T>>
-    implements Comparable<BinaryTreeNode<T>>{
+            implements Comparable<BinaryTreeNode<T>> {
         T value;
 
         BinaryTreeNode<T> parent;
         BinaryTreeNode<T> leftChild;
         BinaryTreeNode<T> rightChild;
-        int heightNode=0;
-        int depthNode=0;
+        int heightNode = 0;
+        int depthNode = 0;
 
         public BinaryTreeNode(T value) {
             this.value = value;
@@ -38,7 +38,7 @@ public class BinaryTree<T extends Comparable<T>> {
         @Override
         public boolean equals(Object o) {
             BinaryTreeNode<T> that = (BinaryTreeNode<T>) o;
-            return this.compareTo(that)==0;
+            return this.compareTo(that) == 0;
         }
 
         @Override
@@ -49,32 +49,33 @@ public class BinaryTree<T extends Comparable<T>> {
 
     private BinaryTreeNode<T> root;
 
-    public BinaryTree(){
-        this.root=null;
+    public BinaryTree() {
+        this.root = null;
     }
 
-    public void insert(T value){
-        if(value == null){
+    public void insert(T value) {
+        if (value == null) {
             throw new IllegalArgumentException();
         }
-        this.root = insert(value,null,root,0);
+        this.root = insert(value, null, root, 0);
     }
+
     //-----------------------------------------------------------------------------
-    public BinaryTreeNode<T> insert(T value,BinaryTreeNode<T> parentNode,BinaryTreeNode<T> node,int d){
-        if(node == null){
+    public BinaryTreeNode<T> insert(T value, BinaryTreeNode<T> parentNode, BinaryTreeNode<T> node, int d) {
+        if (node == null) {
             node = new BinaryTreeNode<>(value);
             node.parent = parentNode;
-            node.depthNode=d;
-            if(parentNode.heightNode == node.heightNode){
-              update(parentNode);
+            node.depthNode = d;
+            if (parentNode.heightNode == node.heightNode) {
+                update(parentNode);
             }
-        }else {
+        } else {
             int compareTo = value.compareTo(node.value);
-            if(compareTo < 0){
-                node.leftChild = insert(value,node,node.leftChild,d+1);
-            }else if(compareTo > 0){
-                node.rightChild = insert(value,node,node.rightChild,d+1);
-            }else {
+            if (compareTo < 0) {
+                node.leftChild = insert(value, node, node.leftChild, d + 1);
+            } else if (compareTo > 0) {
+                node.rightChild = insert(value, node, node.rightChild, d + 1);
+            } else {
                 throw new IllegalArgumentException();
             }
         }
@@ -84,39 +85,40 @@ public class BinaryTree<T extends Comparable<T>> {
 
     //--------------------------------------------------------------------------------
 
-    int depth=0;
-    private BinaryTreeNode<T> find(T value){
-        BinaryTreeNode<T> node = this.root;
-        depth=0;
+    int depth = 0;
 
-        while(node != null){
+    private BinaryTreeNode<T> find(T value) {
+        BinaryTreeNode<T> node = this.root;
+        depth = 0;
+
+        while (node != null) {
             int compareTo = value.compareTo(node.value);
-            if(compareTo < 0){
+            if (compareTo < 0) {
                 node = node.leftChild;
                 depth++;
-            }else if(compareTo > 0){
+            } else if (compareTo > 0) {
                 node = node.rightChild;
                 depth++;
-            }else {
+            } else {
                 break;
             }
         }
         return node;
     }
 
-    public void remove(T value){
+    public void remove(T value) {
         BinaryTreeNode<T> nodeToDelite = find(value);
-        if(nodeToDelite == null){
+        if (nodeToDelite == null) {
             return;
         }
         remove(nodeToDelite);
     }
 
-    public void remove(BinaryTreeNode<T> node){
+    public void remove(BinaryTreeNode<T> node) {
 
-        if(node.leftChild!= null && node.rightChild!= null){
+        if (node.leftChild != null && node.rightChild != null) {
             BinaryTreeNode<T> replacement = node.rightChild;
-            while (replacement.leftChild != null){
+            while (replacement.leftChild != null) {
                 replacement = replacement.leftChild;
             }
             node.value = replacement.value;
@@ -125,41 +127,41 @@ public class BinaryTree<T extends Comparable<T>> {
 
         BinaryTreeNode<T> theChild = node.leftChild != null ? node.leftChild : node.rightChild;
 
-        if(theChild != null){
+        if (theChild != null) {
             theChild.parent = node.parent;
 
-            if(node.parent == null){
-                if(root.heightNode > 1){
-                  theChild.heightNode = root.heightNode;
+            if (node.parent == null) {
+                if (root.heightNode > 1) {
+                    theChild.heightNode = root.heightNode;
                 }
                 root = theChild;
                 root.depthNode = 0;
 
-            }else {
-                if(node.parent.leftChild == node){
+            } else {
+                if (node.parent.leftChild == node) {
                     theChild.depthNode = node.depthNode;
                     node.parent.leftChild = theChild;
 
-                }else {
+                } else {
                     theChild.depthNode = node.depthNode;
                     node.parent.rightChild = theChild;
 
                 }
-                if(node.parent.heightNode - 1 > node.heightNode && needToReverseUpdate(node.parent)){
+                if (node.parent.heightNode - 1 > node.heightNode && needToReverseUpdate(node.parent)) {
                     reverseUpdate(node.parent);
                 }
 
             }
-        }else {
-            if(node.parent == null){
+        } else {
+            if (node.parent == null) {
                 root = null;
-            }else {
-                if(node.parent.leftChild == null){
+            } else {
+                if (node.parent.leftChild == null) {
                     node.parent.leftChild = null;
-                }else {
+                } else {
                     node.parent.rightChild = null;
                 }
-                if(node.parent.heightNode - 1 > node.heightNode && needToReverseUpdate(node.parent)){
+                if (node.parent.heightNode - 1 > node.heightNode && needToReverseUpdate(node.parent)) {
                     reverseUpdate(node.parent);
                 }
             }
@@ -167,53 +169,54 @@ public class BinaryTree<T extends Comparable<T>> {
 
     }
 
-    public String depthAndHeight(T value){
-      BinaryTreeNode<T> node= find(value);
+    public String depthAndHeight(T value) {
+        BinaryTreeNode<T> node = find(value);
         String result = "depth: " + this.depth;
-        maxHeight=0;
-        DFS(0,node);
+        maxHeight = 0;
+        DFS(0, node);
         return result + " Height: " + maxHeight;
     }
 
-    int maxHeight=0;
-    private void DFS(int i,BinaryTreeNode<T> node){
-        if(node == null){
-            if(maxHeight< i){
-                maxHeight=i;
+    int maxHeight = 0;
+
+    private void DFS(int i, BinaryTreeNode<T> node) {
+        if (node == null) {
+            if (maxHeight < i) {
+                maxHeight = i;
             }
             return;
         }
-        if(node.leftChild != null){
-            DFS(i+1,node.leftChild);
-        }else if(node.rightChild != null){
-            DFS(i+1,node.rightChild);
+        if (node.leftChild != null) {
+            DFS(i + 1, node.leftChild);
+        } else if (node.rightChild != null) {
+            DFS(i + 1, node.rightChild);
         }
         return;
     }
 
-    public void update(BinaryTreeNode<T> node){
+    public void update(BinaryTreeNode<T> node) {
         node.heightNode++;
-        if(node.parent != null){
-          if(node.parent.heightNode == node.heightNode ){
-            update(node.parent);
-          }
+        if (node.parent != null) {
+            if (node.parent.heightNode == node.heightNode) {
+                update(node.parent);
+            }
         }
     }
 
-    public void reverseUpdate(BinaryTreeNode<T> node){
+    public void reverseUpdate(BinaryTreeNode<T> node) {
         node.heightNode--;
-        if(node.parent != null){
-            if(node.parent.heightNode - 1 == node.heightNode && needToReverseUpdate(node.parent)){
+        if (node.parent != null) {
+            if (node.parent.heightNode - 1 == node.heightNode && needToReverseUpdate(node.parent)) {
                 update(node.parent);
 
             }
         }
     }
 
-    public boolean needToReverseUpdate(BinaryTreeNode<T> node){
-        maxHeight=0;
-        DFS(0,node);
-        if(maxHeight < node.heightNode){
+    public boolean needToReverseUpdate(BinaryTreeNode<T> node) {
+        maxHeight = 0;
+        DFS(0, node);
+        if (maxHeight < node.heightNode) {
             return true;
         }
         return false;
