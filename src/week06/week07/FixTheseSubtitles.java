ce8746f6edf1d85926284306c1fc10ceafe6681c
georgiPavlov <georgi95.bg@gmail.com>
2016-01-12 14:00:25 +0200

    subs work finish

---
+++ src/week06/week07/FixTheseSubtitles.java
@@ -0,0 +1,49 @@
+package week06.week07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ * Created by georgipavlov on 12.01.16.
+ */
+public class FixTheseSubtitles {
+    public static void fixEncoding(Path path) throws FileNotFoundException, UnsupportedEncodingException {
+        File old =path.toFile();
+        File newFile = new File("this.str");
+        Scanner scanner = new Scanner(old,"Windows-1251");
+        PrintStream p =  new PrintStream(newFile,"UTF-8");
+        while (scanner.hasNextLine()){
+            String t = scanner.nextLine();
+            System.out.println(t);
+            byte[] bytes = t.getBytes(StandardCharsets.UTF_8);
+            String s = new String(bytes, Charset.forName("UTF-8"));
+            p.print(s);
+            p.print(System.lineSeparator());
+        }
+        old.delete();
+        newFile.renameTo(path.toFile());
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Path p = Paths.get("/home/georgipavlov/HackBulgariaProgramming101/lost.s04e11.hdtv.xvid-2hd.srt");
+        try {
+            fixEncoding(p);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
