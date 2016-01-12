5570b6b257c0637b99ea6b2c5f4b7f14eda6b46a
georgiPavlov <georgi95.bg@gmail.com>
2016-01-12 14:10:50 +0200

    subs work finish

---
+++ src/week06/week07/ReduceFilePathIn1Line.java
@@ -0,0 +1,13 @@
+package week06.week07;
+
+import java.nio.file.Path;
+
+/**
+ * Created by georgipavlov on 12.01.16.
+ */
+public class ReduceFilePathIn1Line{
+    public Path reduceFilePath(Path path){
+        return path.normalize();
+    }
+
+}
