3af067b9939a6a8b2b49ec70e9a4ff99195aa390
georgiPavlov <georgi95.bg@gmail.com>
2016-01-17 19:50:03 +0200

    second bonus finish TODO //ADD TEST WITH JUNIT

--- src/week07/TestThisTest.java
+++ src/week07/TestThisTest.java
@@ -6,11 +6,10 @@ import static org.junit.Assert.*;
 /**
  * Created by georgipavlov on 13.01.16.
  */
-public class TestThisTest {
+public class TestThisTest  {
 
     @Test
     public void testA() throws Exception {
-
         assertEquals(1,2);
     }
 
