91a9b43b554db2dec5cbcffa8bc630d6444c7630
georgiPavlov <georgi95.bg@gmail.com>
2016-01-25 21:51:45 +0200

    week 9 starting

--- src/week09/BlockingQueue.java
+++ src/week09/BlockingQueue.java
@@ -1,26 +1,43 @@
 package week09;
 
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Queue;
 
 /**
  * Created by georgipavlov on 25.01.16.
  */
-public class BlockingQueue<T>  {
-    Queue<T> t;
+public class BlockingQueue  {
+    static Queue t = new LinkedList<>();
+    private static final int MAX_SIZE = 5;
 
-    BlockingQueue(){
-        t= new LinkedList<>();
-    }
-    public synchronized void putTo(T addObject){
+
+    public  synchronized void putTo(Object addObject) throws InterruptedException {
+        while (t.size() == MAX_SIZE){
+            System.out.println("I am waiting produser");
+            wait();
+        }
         t.add(addObject);
     }
 
-    public synchronized T pollTo() throws InterruptedException {
+    public synchronized Object pollTo() throws InterruptedException {
         while (t.isEmpty()){
+            System.out.println("Im am waiting consumer");
             wait();
         }
         return t.poll();
+    }
+
+    static class Produser extends Thread{
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
