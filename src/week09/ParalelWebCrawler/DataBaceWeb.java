501bb624bd6f80bb654f8180404b2be2edc7f46e
georgiPavlov <georgi95.bg@gmail.com>
2016-02-01 19:46:47 +0200

    have to fix directory string

--- src/week09/ParalelWebCrawler/DataBaceWeb.java
+++ src/week09/ParalelWebCrawler/DataBaceWeb.java
@@ -3,15 +3,19 @@ package week09.ParalelWebCrawler;
 import org.apache.http.HttpEntity;
 
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * Created by georgipavlov on 01.02.16.
  */
 public class DataBaceWeb {
     protected static  Queue<HttpEntity> entities = new ConcurrentLinkedQueue<>();
-    protected static  Queue<String> link = new ConcurrentLinkedQueue<>();
+    protected static Set<String> link = new CopyOnWriteArraySet<>();
     protected static  String word;
     protected static boolean found = false;
+    protected static final int MAX_THREADS = Runtime.getRuntime().availableProcessors();
+
 
 }
--- src/week09/ParalelWebCrawler/WebCrawler.java
+++ src/week09/ParalelWebCrawler/WebCrawler.java
@@ -1,96 +1,72 @@
 package week09.ParalelWebCrawler;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import week09.BulkThumbnailCreator.*;
+import week09.BulkThumbnailCreator.Producer;
+
+import java.util.ArrayList;
 
 /**
  * Created by georgipavlov on 01.02.16.
  */
-public class WebCrawler {
+public class WebCrawler extends DataBaceWeb implements Runnable{
 
     private static final int MAX_COUNT_OF_PAGES = 15;
     private String word;
     private boolean found = false;
+    private String web;
+    private ArrayList<week09.ParalelWebCrawler.Producer> produsers;
+    private ArrayList<Cosumer> consumers;
 
-    public void start(String startLink,String word) throws IOException {
-        this.word = word;
-        search(0, startLink );
+    public WebCrawler(String word,String web){
+        produsers = new ArrayList<>();
+        consumers = new ArrayList<>();
+        DataBaceWeb.word = word;
+        this.web = web;
     }
 
 
-    private void search(int count,String link) throws IOException {
-        if(count > MAX_COUNT_OF_PAGES){
-            return;
+    @Override
+    public void run() {
+        start();
+        while (!found){}
+        stop();
+
+    }
+
+    private void stop() {
+        for (int i = 0; i <MAX_THREADS ; i++) {
+            produsers.get(i).loop = false;
+            consumers.get(i).loop = false;
         }
-        HashSet<String> results ;
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpget = new HttpGet(link);
-        CloseableHttpResponse response = httpclient.execute(httpget);
-        HttpEntity entity = response.getEntity();
+    }
 
-        if (entity != null) {
-            InputStream in = new BufferedInputStream(entity.getContent());
-            StringBuilder b = new StringBuilder();
-            Scanner scanner = new Scanner(entity.getContent());
-            while (scanner.hasNextLine()){
-                String result = scanner.nextLine();
-                b.append(result);
-                b.append(System.lineSeparator());
-                //System.out.println(result);
-            }
+    private void start() {
+        startProducers();
+        startConsumer();
 
-            if(b.toString().contains(word)){
-                System.out.println("-----------------------------------------------");
-                System.out.println("---------------------- founded ---------------" );
-                System.out.println("-----------------------------------------------");
-                System.out.println( "LINK TO FOLLOW: " +  link );
-                System.out.println("-----------------------------------------------");
-                System.out.println("-----------------------------------------------");
-                System.out.println("-----------------------------------------------");
-                found = true;
-                return;
-            }else {
-                results = getAllLinks(b.toString());
-                for (Iterator<String> it = results.iterator(); it.hasNext();) {
-                    String getLink = it.next();
-                    System.out.println(getLink);
-                    search(count+1,getLink);
-                    if(found){
-                        return;
-                    }
-                }
-            }
-            in.close();
-            response.close();
+    }
+
+    private void startConsumer() {
+        for (int i = 0; i < MAX_THREADS; i++) {
+            Cosumer consumer = new Cosumer();
+            Thread temp = new Thread(consumer);
+            temp.start();
+            consumers.add(consumer);
+
+        }
+
+    }
+    private void startProducers(){
+        for (int i = 0; i < MAX_THREADS; i++) {
+            week09.ParalelWebCrawler.Producer p = new week09.ParalelWebCrawler.Producer();
+            Thread temp = new Thread(p);
+            temp.start();
+            produsers.add(p);
 
         }
     }
 
-
-    public   HashSet<String> getAllLinks(String content) {
-        HashSet<String> resultList = new HashSet<>();
-        String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(content);
-        while (matcher.find()) {
-            if(!matcher.group(1).contains("http")){
-                continue;
-            }
-            resultList.add(matcher.group(1));
-        }
-        return resultList;
-    }
 
 }
+
